47dc2b5aab78fb36d57dcc9905d767d593e3c60e
GeanUribe99 <142021274+GeanUribe99@users.noreply.github.com>
2024-06-11 01:10:28 -0500

    Create Proyect

---
+++ app/src/main/java/com/example/appmovilesec01/Juvilacion.kt
@@ -0,0 +1,17 @@
+package com.example.appmovilesec01
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+
+
+@Composable
+fun activityJuvilacion(){
+
+    Box(modifier = Modifier.fillMaxWidth()){
+        androidx.compose.material3.Text(text = "First Commit")
+    }
+}
